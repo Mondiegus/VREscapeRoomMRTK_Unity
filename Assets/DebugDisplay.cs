85414fcf0a529d52cde0545281f039d52a1d7210
maciej.jarosz <maciej.jarosz@haptology.com>
2022-07-08 07:03:15 +0200

    Working game with gitignore

---
+++ Assets/DebugDisplay.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugDisplay : MonoBehaviour
+{
+    Dictionary<string, string> debugLogs = new Dictionary<string, string>();
+    public Text display;
+
+    private void Update()
+    {
+        Debug.Log(gameObject.name);
+    }
+
+    private void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        if (type == LogType.Log)
+        {
+            string[] splitString = logString.Split(char.Parse(":"));
+            string debugKey = splitString[0];
+            string debugValue = splitString.Length > 1 ? splitString[1] : "";
+
+            if (debugLogs.ContainsKey(debugKey))
+                debugLogs[debugKey] = debugValue;
+            else
+                debugLogs.Add(debugKey, debugValue);
+
+        }
+
+        string displayText = "";
+        foreach (KeyValuePair<string, string> log in debugLogs)
+        {
+            if (log.Value == "")
+                displayText += log.Key + "\n";
+            else
+                displayText += log.Key + ": " + log.Value + "\n";
+        }
+
+        display.text = displayText;
+    }
+}
